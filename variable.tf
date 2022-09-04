87a7095bcf51b3a0abd498882710231a74c4a7f2
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-09-04 12:04:11 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,29 +1,15 @@
 # this is section for variables 
 
-# region 
+# region
 
-variable "region" {
-  default = "us-east-2"
-}
 
 # environment
 
-variable "environment" {
-  default = "Development"
-}
 
 # vpc_cidr 
 
-variable "vpc_cidr" {
-  description = "VPC cidr block"
-}
-
 # public_subnet_1_cidr
 
-variable "public_subnet_1_cidr" {
-  description = "Public Subnet 1 cidr block"
-}
-  
 # public_subnet_2_cidr
 
 # public_subnet_3_cidr
